e73b2145f8e12db2b6ea50dea51af8ae6e81337c
Rashin-Harisi <rashin.harisi@gmail.com>
2024-08-23 10:13:22 +0200

    "correcting imageUrl path"

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
 server.use(router);
-server.listen(443, () => {
+server.listen(3000, () => {
   console.log("JSON Server is running");
 });
 
